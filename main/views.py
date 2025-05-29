74b0754c37defe4bac04bea5fe8555f34df7bab0
Ozodbek2012 <Ozodbek2012@gmail.com>
2025-05-29 09:26:30 +0500

    frist commit

--- main/views.py
+++ main/views.py
@@ -2,11 +2,24 @@ from django.shortcuts import render
 from .models import *
 
 def index_view(request):
+    togri = None
+    notogrilar = None
     soz = request.GET.get('soz')
     if soz is not None:
         soz = soz.lower()
         togrilar = Togri.objects.filter(soz=soz)
         if togrilar.exists():
             togri = togrilar.first()
-            notogrilar = Notogri.objects.filter(togri=togri)
-    return render(request, 'index.html')
+            notogrilar = togri.notogri_set.all()
+        else:
+            notogrilar = Notogri.objects.filter(soz=soz)
+            if notogrilar.exists():
+                notogri = notogrilar.first()
+                togri = notogri.togri
+                notogrilar = togri.notogri_set.all()
+    context = {
+            'togri': togri,
+            'notogrilar': notogrilar,
+            'soz': soz,
+        }
+    return render(request, 'index.html', context)
